732c532b1f632a6476d03eb7c43455dd3abd4fed
@a.shajdullin <a.shajdullin@rko-group.ru>
2026-04-08 23:43:15 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @soft-stack/ui-kit
+
+React UI kit with copy-paste components and themable design tokens.
+
+🚧 Work in progress.
---
+++ apps/book/app/page.tsx
@@ -0,0 +1,9 @@
+import {version} from '@soft-stack/primitives'
+
+export default function Home() {
+  return (
+    <main>
+      Primitives version: {version}
+    </main>
+  );
+}
